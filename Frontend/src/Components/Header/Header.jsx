8fbe2551edc77a361b703b9ad9ce905f889f5d1a
ChiragChrg <chiruchirag2001@gmail.com>
2023-01-26 05:26:18 +0530

    Added Dark variant to Header Component

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 import { CgDarkMode } from 'react-icons/cg'
 import { useContextData } from "../../Hooks/useContextData"
 
-const Header = () => {
+const Header = ({ dark }) => {
     const { setIsDarkTheme } = useContextData();
 
     return (
@@ -12,13 +12,13 @@ const Header = () => {
                 ARMS
             </div>
 
-            <div className="Header-Nav flex gap2">
+            <div className={dark ? "Header-Nav flex gap2 darkTxt" : "Header-Nav flex gap2 lightTxt"}>
                 <Link to="/">Home</Link>
                 <a href="https://devbase.netlify.app/">DevBase</a>
                 <a href="https://github.com/ChiragChrg/ARMS">GitHub</a>
 
                 <div className="Header-ThemeToggle flex" onClick={() => setIsDarkTheme(prev => !prev)}>
-                    <CgDarkMode size={25} color="var(--white)" />
+                    <CgDarkMode size={25} color="inherit" />
                 </div>
             </div>
         </div>
