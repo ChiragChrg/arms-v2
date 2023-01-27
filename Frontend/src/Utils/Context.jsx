40f20620a5e51593f4d0d458b805ce15aef266c3
ChiragChrg <chiruchirag2001@gmail.com>
2023-01-27 21:19:35 +0530

    Fixed theme reset on refresh

--- Frontend/src/Utils/Context.jsx
+++ Frontend/src/Utils/Context.jsx
@@ -1,12 +1,26 @@
-import { useState, createContext } from 'react';
+import { useState, useEffect, createContext } from 'react';
 
 export const Context = createContext();
 
 const ContextProvider = ({ children }) => {
     const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
     const [isUserFaculty, setIsUserFaculty] = useState(false);
-    const [isDarkTheme, setIsDarkTheme] = useState(false);
     const [onLogout, setOnLogout] = useState(false);
+
+    // isDarkTheme reverts back to false on refresh
+    const [isDarkTheme, setIsDarkTheme] = useState(false);
+    // So skip useEffect theme block for the first render
+    const [initialRender, setInitialRender] = useState(false);
+
+
+    useEffect(() => {
+        if (initialRender) {
+            document.body.setAttribute("data-theme", isDarkTheme ? "dark" : "light");
+            localStorage.setItem("arms-theme", isDarkTheme ? "dark" : "light");
+        } else {
+            setInitialRender(true);
+        }
+    }, [isDarkTheme])
 
     return (
         <Context.Provider value={{
