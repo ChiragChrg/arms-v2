4e338ee9d0669b6542522692f0bbd25ebce2091a
ChiragChrg <chiruchirag2001@gmail.com>
2023-01-27 21:18:30 +0530

    Added Position prop & Disable Anime on Trails Comp

--- Frontend/src/Components/Trails/Trails.jsx
+++ Frontend/src/Components/Trails/Trails.jsx
@@ -1,23 +1,35 @@
 import "./Trails.css"
-import { useEffect, useRef } from "react"
+import { useState, useEffect, useRef } from "react"
+import { MdPlayArrow, MdPlayDisabled } from "react-icons/md"
 
-const Trails = ({ angle, center }) => {
+const Trails = ({ angle, position }) => {
+    const [disableAnimation, setDisableAnimation] = useState(false);
     const StripCount = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     const TrailsRef = useRef();
 
     useEffect(() => {
         if (angle) TrailsRef.current.style.transform = `rotate(${angle}deg)`;
-        if (center) TrailsRef.current.style.right = "0";
-    }, [angle, center])
+        if (position == "center") TrailsRef.current.style.right = "0";
+        if (position == "left") TrailsRef.current.style.right = "20%";
+    }, [angle, position])
 
     return (
-        <div className="Trails-Main" ref={TrailsRef}>
-            <div className="Trails-Holder flex col gap2">
-                {StripCount.map((item) => {
-                    return <div key={item} className="Trails-Trail flex"></div>
-                })}
+        <>
+            <div className="Trails-Main" ref={TrailsRef}>
+                <div className="Trails-Holder flex col gap2">
+                    {StripCount.map((item) => {
+                        return <div key={item} data-disableAnime={disableAnimation} className="Trails-Trail flex"></div>
+                    })}
+                </div>
             </div>
-        </div>
+
+            <div className="Trails-Disable flex" onClick={() => setDisableAnimation(prev => !prev)} title={disableAnimation ? "Play Animation" : "Disable Animation"}>
+                {disableAnimation ?
+                    <MdPlayDisabled size={25} color="var(--primary)" />
+                    : <MdPlayArrow size={25} color="var(--primary)" />
+                }
+            </div>
+        </>
     )
 }
 
