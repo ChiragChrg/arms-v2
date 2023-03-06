bfbec0c6b7418f7a0ada089b70436ccdd923fa69
ChiragChrg <chiruchirag2001@gmail.com>
2023-03-06 21:43:44 +0530

    Institution Page Done

---
+++ Frontend/src/Pages/Institution/Institution.jsx
@@ -0,0 +1,73 @@
+import "./Institution.css"
+import { useState, useEffect } from "react"
+import NavRoute from "../../Components/NavRoute/NavRoute"
+import axios from "axios"
+import { Link } from "react-router-dom"
+
+// import { FolderSVG } from "../../Assets"
+import Skeleton from '@mui/material/Skeleton';
+import { FiPlus } from "react-icons/fi"
+import { BsBuilding } from "react-icons/bs"
+
+const Institution = () => {
+    const [institutions, setInstitutions] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        const GetInstitutions = async () => {
+            setLoading(true);
+            try {
+                const res = await axios.get('/api/getinstitutions')
+                if (res.status === 200) {
+                    setInstitutions(res.data.DocsDB)
+                    setLoading(false);
+                }
+            } catch (err) {
+                console.log(err)
+                setLoading(false);
+            }
+        }
+        GetInstitutions();
+    }, [])
+
+    return (
+        <div className="Institution-Main">
+            <NavRoute routes={["Institution"]} />
+            <div className="Institution-Header flex">
+                <h1>Institutions</h1>
+
+                <Link to="new" className="Institution-Create flex gap05">
+                    <FiPlus size={25} color="inherit" />
+                    <span>New Institution</span>
+                </Link>
+            </div>
+
+            {!loading ? <div className="Institution-Cards">
+                {institutions.map((obj, index) => {
+                    return <Link key={index} state={obj} to={`${obj.collegeName.replaceAll(" ", "-").toLowerCase()}`} className="Institution-Card flex col">
+                        <div className="Institution-Icon flex">
+                            <BsBuilding size={30} color="var(--white)" />
+                        </div>
+                        <h2>{obj.collegeName}</h2>
+                        <p>{obj.description}</p>
+                    </Link>
+                })}
+            </div>
+                :
+                <div className="Institution-Cards">
+                    <Skeleton variant="rounded" animation="wave" width={324} height={145} />
+                    <Skeleton variant="rounded" animation="wave" width={324} height={145} />
+                    <Skeleton variant="rounded" animation="wave" width={324} height={145} />
+                    <Skeleton variant="rounded" animation="wave" width={324} height={145} />
+                    <Skeleton variant="rounded" animation="wave" width={324} height={145} />
+                    <Skeleton variant="rounded" animation="wave" width={324} height={145} />
+                    <Skeleton variant="rounded" animation="wave" width={324} height={145} />
+                    <Skeleton variant="rounded" animation="wave" width={324} height={145} />
+                    <Skeleton variant="rounded" animation="wave" width={324} height={145} />
+                </div>
+            }
+        </div>
+    )
+}
+
+export default Institution
---
+++ Frontend/src/Pages/Institution/NewInstitute.jsx
@@ -0,0 +1,87 @@
+import "./NewInstitute.css"
+import { useState, useRef } from 'react'
+import { useNavigate } from "react-router-dom"
+import { useContextData } from "../../Hooks/useContextData"
+import axios from 'axios'
+import { toast } from "react-toastify"
+import NavRoute from "../../Components/NavRoute/NavRoute"
+import LoaderBtn from "../../Components/LoaderBtn/LoaderBtn"
+
+import { BooksSVG } from "../../Assets"
+import { MdShortText } from "react-icons/md"
+import { FiUser } from "react-icons/fi"
+import { BsBuilding } from "react-icons/bs"
+
+const NewInstitute = () => {
+    const [loading, setLoading] = useState(false);
+    const { userData } = useContextData();
+    const navigate = useNavigate();
+
+    const CollegeRef = useRef();
+    const DescRef = useRef();
+    // console.log(userData)
+
+    const HandleSubmit = async (e) => {
+        e.preventDefault();
+        setLoading(true)
+
+        const data = {
+            collegeName: CollegeRef.current.value,
+            description: DescRef.current.value,
+            userName: userData.username,
+        }
+
+        try {
+            const result = await axios.post("/api/createinstitute", data)
+            if (result.status == 200) {
+                setLoading(false)
+                toast.success(result.data.message)
+                navigate('/institution')
+            }
+        } catch (err) {
+            setLoading(false)
+            toast.error(err.message)
+            console.log(err)
+        }
+    }
+    return (
+        <div className="NewInstitute-Main">
+            <NavRoute routes={["Institution", "Institution/New"]} />
+            <h1>Create a new <span style={{ color: "var(--primary)" }}>Institute</span></h1>
+
+            <div className="NewInstitute-Container flex">
+                <form className="NewInstitute-Form flex col gap" onSubmit={HandleSubmit}>
+                    <div className="NewInstitute-InputHolder flex col">
+                        <label htmlFor="email">Institute Name</label>
+                        <div className="NewInstitute-Input flex gap05">
+                            <input type="text" maxLength={30} placeholder="Enter College Name" ref={CollegeRef} required />
+                            <BsBuilding size={25} color="var(--grey)" />
+                        </div>
+                    </div>
+
+                    <div className="NewInstitute-InputHolder flex col">
+                        <label htmlFor="email">Description</label>
+                        <div className="NewInstitute-Input flex gap05">
+                            <textarea rows={3} maxLength={60} placeholder="Enter College description" ref={DescRef} required />
+                            <MdShortText size={25} color="var(--grey)" />
+                        </div>
+                    </div>
+
+                    <div className="NewInstitute-InputHolder flex col">
+                        <label htmlFor="email">Institute Creator</label>
+                        <div className="NewInstitute-Input flex gap05">
+                            <input type="text" style={{ color: "var(--grey)" }} defaultValue={userData.username} disabled required />
+                            <FiUser size={25} color="var(--grey)" />
+                        </div>
+                    </div>
+
+                    <LoaderBtn type="submit" className="NewInstitute-Submit flex" loading={loading} iconType="plus" text="Create" />
+                </form>
+
+                <img src={BooksSVG} className="NewInstitute-Vector" alt="BooksSVG" width={400} />
+            </div>
+        </div>
+    )
+}
+
+export default NewInstitute
