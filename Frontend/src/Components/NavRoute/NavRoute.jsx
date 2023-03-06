4983b3bbd189fbc96de4e507fad6e0a644d557ee
ChiragChrg <chiruchirag2001@gmail.com>
2023-03-06 21:42:48 +0530

    NavRouter Nested route support

--- Frontend/src/Components/NavRoute/NavRoute.jsx
+++ Frontend/src/Components/NavRoute/NavRoute.jsx
@@ -1,30 +1,32 @@
 import "./NavRoute.css"
 import { Fragment } from 'react'
-import { NavLink } from "react-router-dom"
-import { HiOutlineHome } from 'react-icons/hi'
+import { Link } from "react-router-dom"
 import { IoIosArrowForward } from 'react-icons/io'
 
-const NavRoute = ({ routes }) => {
-    // const NavArray = [
-    //     "Bruhh",
-    //     "About",
-    //     "Contact"
-    // ]
+const NavRoute = ({ routes, defaultRoute = "Dashboard", state = {} }) => {
+    //Custome made Route Map
+
+    //Default Root Path is "Dashboard", can be changed using @defaultRoute prop 
+
+    //Send an array of String which is also your path to different components
+    //// example: ["Product","Details","Payment"] etc.
+
+    //If You have a route within the same parent, include the parent with "/"
+    //// example: ["Institutes","Institutes/New"], the path will be "institute/new" & lable is "New"
 
     return (
         <div className="NavRoute-Main flex">
-            <NavLink to="/dashboard">
-                {/* <HiOutlineHome size={17} color="inherit" /> */}
-                <span>Dashboard</span>
-            </NavLink>
+            <Link to={`/${defaultRoute.toLowerCase()}`}>
+                <span>{defaultRoute}</span>
+            </Link>
 
             {routes.map((item, index) => {
                 return (
                     <Fragment key={index}>
                         <IoIosArrowForward size={18} color="var(--grey)" />
-                        <NavLink to={`/${item.toLowerCase()}`}>
-                            <span>{item}</span>
-                        </NavLink>
+                        <Link to={`/${item.toLowerCase()}`} state={state}>
+                            <span>{item.includes("/") ? item.split("/").pop() : item}</span>
+                        </Link>
                     </Fragment>
                 )
             }
