b6fc748df2fd04626f2b95de814a9ba7e96d6832
ChiragChrg <chiruchirag2001@gmail.com>
2023-03-27 02:13:05 +0530

    Fix crash on urls containing dots

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+//Fix JWT dot url error
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), pluginRewriteAll()],
 })
